aaa59748c37c5179f7abdf76ead6db7500a48457
Enrique Gutierrez <khike.gtz@gmail.com>
2024-09-12 22:01:08 -0600

    Se suben ejercicios

--- src/Numeros.java
+++ src/Numeros.java
@@ -1,4 +1,3 @@
-
 public class Numeros {
 	public static void main(String[] args) {
 		for(int x = 1; x <=3;  x++) {
--- src/triangulosreversa.java
+++ src/triangulosreversa.java
@@ -2,5 +2,15 @@
 public class triangulosreversa {
 	public static void main(String[] args) {
 		
+		int i, j;
+		
+		for(i= 6; i>=1; i--) {
+			for( j=1; i>=j; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
 	}//main
 }//class trianguloreversa
